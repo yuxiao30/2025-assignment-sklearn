0c0661c9e08319ef1bf82c0dd391cde7b01ce1f6
Xiao Yu <yuxiao10086@gmail.com>
2025-12-19 17:10:39 +0100

    FIX flake8

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,59 +1,10 @@
-"""Assignment - making a sklearn estimator and cv splitter.
-
-The goal of this assignment is to implement by yourself:
-
-- a scikit-learn estimator for the KNearestNeighbors for classification
-  tasks and check that it is working properly.
-- a scikit-learn CV splitter where the splits are based on a Pandas
-  DateTimeIndex.
-
-Detailed instructions for question 1:
-The nearest neighbor classifier predicts for a point X_i the target y_k of
-the training sample X_k which is the closest to X_i. We measure proximity with
-the Euclidean distance. The model will be evaluated with the accuracy (average
-number of samples corectly classified). You need to implement the `fit`,
-`predict` and `score` methods for this class. The code you write should pass
-the test we implemented. You can run the tests by calling at the root of the
-repo `pytest test_sklearn_questions.py`. Note that to be fully valid, a
-scikit-learn estimator needs to check that the input given to `fit` and
-`predict` are correct using the `validate_data, check_is_fitted` functions
-imported in this file.
-You can find more information on how they should be used in the following doc:
-https://scikit-learn.org/stable/developers/develop.html#rolling-your-own-estimator.
-Make sure to use them to pass `test_nearest_neighbor_check_estimator`.
-
-
-Detailed instructions for question 2:
-The data to split should contain the index or one column in
-datatime format. Then the aim is to split the data between train and test
-sets when for each pair of successive months, we learn on the first and
-predict of the following. For example if you have data distributed from
-november 2020 to march 2021, you have have 4 splits. The first split
-will allow to learn on november data and predict on december data, the
-second split to learn december and predict on january etc.
-
-We also ask you to respect the pep8 convention: https://pep8.org. This will be
-enforced with `flake8`. You can check that there is no flake8 errors by
-calling `flake8` at the root of the repo.
-
-Finally, you need to write docstrings for the methods you code and for the
-class. The docstring will be checked using `pydocstyle` that you can also
-call at the root of the repo.
-
-Hints
------
-- You can use the function:
-
-from sklearn.metrics.pairwise import pairwise_distances
-
-to compute distances between 2 sets of samples.
-"""
 import numpy as np
 import pandas as pd
 
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.model_selection import BaseCrossValidator
+from sklearn.utils.multiclass import type_of_target
 from sklearn.utils.validation import check_is_fitted, validate_data
 
 
@@ -66,17 +17,7 @@ class KNearestNeighbors(ClassifierMixin, BaseEstimator):
     def fit(self, X, y):
         """Store training data.
 
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            Training samples.
-        y : array-like of shape (n_samples,)
-            Target labels.
-
-        Returns
-        -------
-        self : object
-            Fitted estimator.
+        Raises a ValueError when passed a continuous target.
         """
         X, y = validate_data(self, X, y=y, reset=True)
 
@@ -85,37 +26,31 @@ class KNearestNeighbors(ClassifierMixin, BaseEstimator):
         if self.n_neighbors <= 0:
             raise ValueError("n_neighbors must be >= 1.")
 
+        target_type = type_of_target(y)
+        if target_type == "continuous":
+            raise ValueError(
+                "continuous target is not supported for classification"
+            )
+        if target_type not in ("binary", "multiclass"):
+            raise ValueError(f"Unknown label type: {target_type}")
+
         self.X_ = X
         self.y_ = y
         self.classes_ = np.unique(y)
         return self
 
     def predict(self, X):
-        """Predict class labels for samples in X.
-
-        Parameters
-        ----------
-        X : array-like of shape (n_test_samples, n_features)
-            Test samples.
-
-        Returns
-        -------
-        y_pred : ndarray of shape (n_test_samples,)
-            Predicted labels.
-        """
+        """Predict class labels for samples in X."""
         check_is_fitted(self, attributes=["X_", "y_", "classes_"])
         X = validate_data(self, X, reset=False)
 
         n_train = self.X_.shape[0]
         k = min(int(self.n_neighbors), n_train)
 
-        # Distances shape: (n_test, n_train)
         distances = pairwise_distances(X, self.X_, metric="euclidean")
         neigh_idx = np.argsort(distances, axis=1)[:, :k]
         neigh_y = self.y_[neigh_idx]
 
-        # Majority vote with deterministic tie-break:
-        # choose the smallest class (same as argmax over bincount).
         class_to_int = {c: i for i, c in enumerate(self.classes_)}
         neigh_int = np.vectorize(class_to_int.get, otypes=[int])(neigh_y)
 
@@ -148,7 +83,7 @@ class MonthlySplit(BaseCrossValidator):
         self.time_col = time_col
 
     def __repr__(self):
-        # Test expects EXACT string formatting with single quotes.
+        """Return repr used by tests."""
         return f"MonthlySplit(time_col='{self.time_col}')"
 
     def _get_time_values(self, X):

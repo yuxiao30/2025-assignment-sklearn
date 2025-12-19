7a0aadb95118639a0d200b96ddf1484b295416fa
Xiao Yu <yuxiao10086@gmail.com>
2025-12-19 17:13:32 +0100

    FIX flake8

--- sklearn_questions.py
+++ sklearn_questions.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.model_selection import BaseCrossValidator
